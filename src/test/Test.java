2ea583f05e6f54ebaecd1d020c13816d1a756cbd
miao <574702257@qq.com>
2019-05-22 20:17:55 +0800

    get some tests

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.apache.log4j.Logger;
 
 import clientInfo.OneClient;
@@ -7,7 +11,7 @@ import clientInfo.OneClient;
 public class Test
 {
 	private static Logger logger = Logger.getLogger(Test.class);
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 		// TODO Auto-generated method stub
 //		try
@@ -46,9 +50,33 @@ public class Test
 //			// TODO: handle exception
 //			e.printStackTrace();
 //		}
-        logger.info("This is info message.");  
+//        logger.info("This is info message.");  
+		
+		String str = "12345";
+		MessageDigest digest =MessageDigest.getInstance("MD5");
 
+		byte[] bs = digest.digest(str.getBytes());
+		OneClient.BytesPrint("未经过散列的S:",bs);
+		String bb = "";
+		for(int i =0;i<bs.length;i++)
+		{
+			bb += byteHEX(bs[i]);
+		}
+		
+		String aa = new String(bs, "iso-8859-1");	
+		System.out.println(bb);
+		
+		
 
 	}
 
+	public static String byteHEX(byte ib)
+	{
+		char[] Digit ={ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		char[] ob = new char[2];
+		ob[0] = Digit[(ib >>> 4) & 0X0F];
+		ob[1] = Digit[ib & 0X0F];
+		String s = new String(ob);
+		return s;
+	}
 }
